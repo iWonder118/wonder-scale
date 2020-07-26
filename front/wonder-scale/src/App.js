0205cf2de369edcc1d511cf46a0323d0458eb8ba
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2020-07-26 23:02:49 +0900

    First commit

---
+++ front/wonder-scale/src/App.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+const server = 'http://localhost:8000/hello';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      status: false,
+      result: '',
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    axios.get(server)
+    .then((res) => {
+      this.setState({
+        status: true,
+        result: res.data
+      });
+      console.log(this.state);
+    })
+    .catch((e) => {
+      console.error(e);
+      this.setState({
+        status: false,
+        result: e,
+      });
+    });
+  }
+
+  
+  render() {
+    const result = (this.state.status) ? (<div>{this.state.result.message}</div>) : (<div>Not Yet</div>);
+    return (
+      <div>
+        <button onClick={this.handleClick}> Get Data</button>
+        {result}
+      </div>
+    );
+  }
+}
+
+export default App;
